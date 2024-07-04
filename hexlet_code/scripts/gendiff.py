565b63fa2f090ac8d42b150eeef8367a920dd360
olga1webb <olga1webb@gmail.com>
2024-07-04 14:24:02 +0300

    function generate_diff works properly while runned in python

--- hexlet_code/scripts/gendiff.py
+++ hexlet_code/scripts/gendiff.py
@@ -1,10 +1,65 @@
 #!/usr/bin/env python3
-
+import json
 import argparse
-parser = argparse.ArgumentParser(
-		description="Compares two configuration files and shows a difference.")
-parser.add_argument("first_file")
-parser.add_argument("second_file")
-parser.add_argument('-f', '--format', help='set format of output') 
-args = parser.parse_args()
-print(args.echo)
+
+def first_char(word):
+	return word[2] if len(word) > 2 else ''
+
+
+def generate_diff(file_path1, file_path2):
+    result = ''
+    list_result = list()
+    output_result = '{\n'
+    try:
+        with open(file_path1, 'r') as file1, open(file_path2, 'r') as file2:
+            dict1 = json.load(file1)
+            dict2 = json.load(file2)
+    except FileNotFoundError as e:
+        return f"Error: File not found - {e}"
+    except json.JSONDecodeError as e:
+        return f"Error: Invalid JSON - {e}"
+
+    for a in dict1.keys():
+        if a not in dict2.keys():
+            result = f'- {a}: {dict1[a]}'
+            list_result.append(result)
+
+        elif dict1[a] == dict2[a]:
+            result = f'  {a}: {dict1[a]}'
+            list_result.append(result)
+            dict2.pop(a)
+
+        elif dict1[a] != dict2[a]:
+            result1 = f'- {a}: {dict1[a]}'
+            result2 = f'+ {a}: {dict2[a]}'
+            list_result.append(result1)
+            list_result.append(result2)
+            dict2.pop(a)
+    for b in dict2.keys():
+        result = f'+ {b}: {dict2[b]}'
+        list_result.append(result)
+
+    sorted_result = sorted(list_result, key=first_char)
+    for item in sorted_result:
+    	output_result += item + '\n'
+    return output_result + '}'
+
+
+def parser_diff():
+	parser = argparse.ArgumentParser(
+			description="Compares two configuration files and shows a difference.")
+	parser.add_argument("first_file")
+	parser.add_argument("second_file")
+	parser.add_argument('-f', '--format', help='set format of output') 
+	return parser.parse_args()
+	
+
+def main():
+	args = parser_diff()
+	diff = generate_diff(args.first_file, args.second_file)
+	print(diff)
+	
+
+if __name__ == '__main__':
+    main()
+
---
+++ hexlet_code/scripts/generate.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+from gendiff import generate_diff
+
+diff = generate_diff(file_path1, file_path2)
+print(diff)
