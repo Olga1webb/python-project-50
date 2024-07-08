00dcc29561c9414e9307283bfc6f816064891754
olga1webb <olga1webb@gmail.com>
2024-07-08 13:34:11 +0300

    added tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 from hexlet_code.scripts.gendiff import generate_diff
 
-expected_result = """{
+
+def test_generate_diff():
+	expected_result = """{
   - follow: False
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -9,6 +11,31 @@ expected_result = """{
   + timeout: 20
   + verbose: True
 }"""
-def test_generate_diff():
 	actual_result = generate_diff("file1.json", "file2.json")
+	assert actual_result == expected_result
+
+def test_same_files():
+	expected_result = """{
+    follow: False
+    host: hexlet.io
+    proxy: 123.234.53.22
+    timeout: 50
+}"""
+	actual_result = generate_diff("file1.json", "file1.json")
+	assert actual_result == expected_result
+
+def test_one_empty():
+	expected_result = """{
+  - follow: False
+  - host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+}"""
+	actual_result = generate_diff("file1.json", "file0.json")
+	assert actual_result == expected_result
+
+def test_both_empty():
+	expected_result = """{
+}"""
+	actual_result = generate_diff("file0_copy.json", "file0.json")
 	assert actual_result == expected_result
